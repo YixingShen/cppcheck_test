a119678a45996f989c75b139689f45dff0a19795
YixingShen <kkman3651878@hotmail.com>
2024-03-18 02:06:27 +0800

    add

--- run.bat
+++ run.bat
@@ -5,22 +5,22 @@ set addon=--addon=misra/misra.json
 cd cppchk
 mkdir build-dir
 
-cppcheck --verbose --enable=all ^
---cppcheck-build-dir=build-dir ^
---check-library ^
---check-level=normal ^
+cppcheck --verbose ^
+--language=c --language=c++ ^
+--std=c99 --std=c++11 ^
+--enable=all ^
+--disable=missingInclude ^
 --library=gnu ^
 --library=cfg/arm-cortex-m.cfg ^
 --library=cfg/gnu.cppcheck.cfg ^
---library=cfg/armv7m.cppcheck.cfg  ^
+--library=cfg/armv8.1-m.main.cppcheck.cfg  ^
 --library=cfg/gnu.cppcheck.cfg ^
 --platform=platforms/arm32-wchar_t4 ^
 --inconclusive ^
 --inline-suppr ^
---language=c --language=c++ ^
---std=c99 --std=c++11 ^
 --suppress=missingIncludeSystem ^
---disable=missingInclude ^
+--check-library --check-level=normal ^
+--cppcheck-build-dir=build-dir ^
 --showtime=none ^
 %addon% ../MISRA-Example-Suite --xml 2> report.xml
 ::%addon% ../MISRA-Example-Suite --output-file=report.txt
